2a268ea26ada192db2a658500234566993d49fb1
Benoit Malenfant <benmalenfant@gmail.com>
2022-07-27 18:05:10 -0700

    CleanUp - Folder Structure

---
+++ SW_Repartisseur/inc/slmx4_vcom.h
@@ -0,0 +1,62 @@
+#ifndef SENSOR_DEF
+#define SENSOR_DEF
+
+#include <iostream>
+#include <errno.h> // Error integer and strerror() function
+#include <fcntl.h> // Contains file controls like O_RDWR
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h> // Contains POSIX terminal control definitions
+#include <unistd.h> // write(), read(), close()
+#include <stdint.h>
+
+#include "serialib.h"
+#include "Proto_Pulmo.h"
+
+//#define SERIAL_PORT "/dev/ttyACM0"
+
+#define SERIAL_PORT "/dev/serial/by-id/usb-NXP_SEMICONDUCTORS_MCU_VIRTUAL_COM_DEMO-if00"
+
+class slmx4
+{
+
+public:
+	slmx4();
+
+	serialib serial;
+
+	void Begin();
+	void End();
+	void GetFrameNormalized();
+	void GetFrameRaw();
+
+	int isOpen;
+
+	void OpenRadar();
+	void CloseRadar();
+
+	int check_ACK();
+
+#ifndef DEBUG
+private:
+#endif
+
+	enum cmds{rx_wait,frame_start,frame_end,ddc_en};
+
+	void init_device();
+	void init_serial();
+	void updateNumberOfSamplers();
+	void Iterations();
+
+	void TryUpdateChip(int);
+
+	int status;
+	int numSamplers;
+
+
+
+};
+
+
+#endif
